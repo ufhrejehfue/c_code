77dfbe975fbb6eb6a33cc03db8f67518bbba999b
leonar <3230746861@qq.com>
2024-07-19 21:46:11 +0800

    Update 计算器.cpp

--- 计算器.cpp
+++ 计算器.cpp
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int Add(int x, int y)
 {
-
 	return (x+y);
 }
-
 int Sub(int x, int y)
 {
 	return (x - y);
@@ -16,17 +14,8 @@ int Mul(int x, int y)
 int Div(int x, int y) 
 {
 	return (x / y);
-}
-void cal(int (*pf)(int, int))
-{
-	int x = 0;
-	int y = 0;
-	int ret = 0;
-	printf_s("请输入两个操作数： \n");
-	scanf_s("%d %d", &x, &y);
-	ret = pf(x, y);
-	printf_s("结果为：%d \n", ret);
-}
+} 
+
 void menu()
 {
 	printf_s("***********************************\n");
@@ -35,36 +24,30 @@ void menu()
 	printf_s("*************0.exit********\n");
 	printf_s("***********************************\n");
 }
+
 int main()
 {
-
+	int x = 0;
+	int y = 0;
+	int ret = 0;
+    int (*pfArr[5]) (int,int)={0,Add,Sub,Mul,Div};
 	int input = 0;
-	do 
+	printf_s("请选择： ");
+	scanf_s("%d", &input);
+	if(input==0)
 	{
-		menu();
-		printf_s("请选择： ");
-		scanf_s("%d", &input);
-		switch (input)
-		{
-		case 1:
-			cal(Add);
-			break;
-		case 2:
-			cal(Sub);
-			break;
-		case 3:
-			cal(Mul);
-			break;
-		case 4:
-			cal(Div);
-			break;
-		case 0:
-			printf_s("退出计算器\n");
-			break;
-		default:
-			printf_s("选择错误\n");
-			break;
-		}
-	} while (input);
+		printf_s("退出计算器  \n");
+	}
+	else if(input>=1&&input<=4)
+	{
+      printf_s("请输入两个操作数： \n");
+	scanf_s("%d %d", &x, &y);
+	ret = pfArr[input](x, y);
+	printf_s("结果为：%d \n", ret);
+	}
+	else
+	{
+     printf_s("选择错误 \n");
+	}
 return 0;
 }
