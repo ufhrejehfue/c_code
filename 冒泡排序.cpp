62ac0d9a46bad36d1d86872c3aaf970aa824a90b
leonar <3230746861@qq.com>
2024-07-22 22:33:05 +0800

    Update 冒泡排序.cpp

--- 冒泡排序.cpp
+++ 冒泡排序.cpp
@@ -5,7 +5,6 @@ int cmp_int(const void* e1, const  void* e2)
 	return (*(int*)e1 - *(int*)e2);
 }
 
-
 void bubble_sort(void*base,int sz,int width,int(*cmp)(const void*e1,const void*e2))
 {
    for(int i=0;i<sz-1;i++)
@@ -37,6 +36,7 @@ void Swap(char*buf1,char*buf2,int width)
 	buf2++;
   }
 }
+
 int main()
 {
 	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
